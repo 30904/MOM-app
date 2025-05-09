21f7052181af0319143c8920a17f0e8e1e615967
atharva-gite <atharva.gite@spit.ac.in>
2025-05-10 00:57:25 +0530

    commit3

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-import eventlet
-eventlet.monkey_patch()
-
 from app import create_app, socketio
 from config import Config
 
@@ -8,12 +5,10 @@ app = create_app(Config)
 
 if __name__ == '__main__':
     try:
-        # Use eventlet's WSGI server
         socketio.run(app,
                     debug=True,
                     host='0.0.0.0',
-                    port=5000,
-                    use_reloader=True,
-                    log_output=True)
+                    port=8080,
+                    allow_unsafe_werkzeug=True)
     except Exception as e:
         print(f"Error: {e}") 
